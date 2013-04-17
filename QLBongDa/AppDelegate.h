570cddba5ef4c9770b937c0e7805c7bd1925dd41
kiennd <kien.nguyenduc08@gmail.com>
2013-04-17 20:13:00 +0700

    Initial Commit

---
+++ QLBongDa/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  QLBongDa
+//
+//  Created by KIENND on 4/17/13.
+//  Copyright (c) 2013 KIENND. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
